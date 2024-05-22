3ed4db48049582bce26a490a9621568ae8f07391
r00cha <roocha99pt@gmail.com>
2024-05-23 00:05:57 +0100

    checking if it's fixed

--- src/main/pt/up/fe/comp2024/ast/TypeUtils.java
+++ src/main/pt/up/fe/comp2024/ast/TypeUtils.java
@@ -37,7 +37,7 @@ public class TypeUtils {
             case LENGTH_LITERAL -> getVarType("length", getMethodName(expr), table);
             case INTEGER_LITERAL -> new Type(INT_TYPE_NAME, false);
             case ARRAY_ACCESS_EXPR -> new Type(getVarType(expr.getChild(0).get("name"), getMethodName(expr), table).getName(), false); // because the access is an int or a boolean
-            case NEW_ARRAY_EXPR -> new Type(expr.get("name"), true);
+            //case NEW_ARRAY_EXPR -> new Type(expr.get("name"), true);
             case ARRAY_INIT_EXPR -> {
                 JmmNode firstElement = expr.getChildren().get(0);
                 Type firstElementType = getExprType(firstElement, table);
